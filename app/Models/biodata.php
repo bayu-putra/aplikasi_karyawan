315bc8b877906fe8b09b85707be839a307b2505d
bayuputra90 <bputra2212@gmail.com>
2023-06-30 14:15:26 +0700

    create relationship

--- app/Models/biodata.php
+++ app/Models/biodata.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class biodata extends Model
 {
     use HasFactory;
+
+    function pendidikan() : HasMany
+    {
+        return $this->hasMany(pendidikan::class, 'biodata_id');
+    }
 }
--- app/Models/pendidikan.php
+++ app/Models/pendidikan.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class pendidikan extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the biodata that owns the pendidikan
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function biodata(): BelongsTo
+    {
+        return $this->belongsTo(biodata::class, 'biodata_id');
+    }
 }
